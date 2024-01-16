d0e2bb9291c465e270b926970e122ef811145f3b
Yo-So-Ku <152880521+Yo-So-Ku@users.noreply.github.com>
2024-01-16 10:11:13 +0000

    queue 2

--- Data_Structure/Queue_2.py
+++ Data_Structure/Queue_2.py
@@ -1,37 +1,50 @@
 #ceate a looping queue
 Queue = ["","","","",""]
-
+print("working?_")
 #Setting variables
 front = 0
 rear = -1
 size = 0
+full = 0
+printer = 0
 
 #creating subroutines
 def enqueue(name):
-    global size, rear
-    if size != 5:
+    global size, rear, full
+    if (size < 5) or (full == 5):
         rear = rear + 1 
         if rear == 5:
             rear = rear - 5
         Queue[rear] = name
         size = size + 1
-    elif size == 5:
-        print("List if full!")
+    
+    if size == 5:
+       full = 5
+    
+    if full == 5:
+        size = 5
+
 
 def dequeue():
     global size, front
     if size != 0:
         front = front + 1
         size = size -1
-    if size == 0:
-        print("list is empty!")
+    
 
 def showlist():
+    global size, front, printer
+    printer = front
+    if size == 0:
+        print("there is no list")
     for i in range(size):
-        x= i + front
-        if x == 5:
-            x=x-5
-        print(Queue[i + front])
+        print(Queue[printer])
+        printer = printer + 1
+        if printer == 5:
+            printer = printer - 5
+
+
+    
 
 #Testing
 
@@ -40,7 +53,5 @@ enqueue("b")
 enqueue("c")
 enqueue("d")
 enqueue("e")
-enqueue("f")
-enqueue("g")
-enqueue("h")
+dequeue()
 showlist()
